6edf094ee1f32a34107e2d3b1d53a47d237c5a68
Thiago Pereira <titiura@gmail.com>
2023-08-27 11:59:10 -0300

    feat: edit main.tf day-1

--- day-1/main.tf
+++ day-1/main.tf
@@ -1,13 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-  
-
-terraform {
-  backend "s3" {
-    bucket = "bucket-learming-terraform"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-    
-}
