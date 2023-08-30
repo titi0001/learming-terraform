a11074562e27dceba564ef2313bec101b9ca2a73
Thiago Pereira <titiura@gmail.com>
2023-08-30 14:37:24 -0300

    feat: add multiple providers

--- day-1/ec2.tf
+++ day-1/ec2.tf
@@ -17,3 +17,25 @@ resource "aws_instance" "web" {
     Name = "terraform-example-ec2"
   }
 }
+
+data "aws_ami" "ubuntu-east-2" {
+  provider = aws.us-east-2
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web-east-2" {
+  provider = aws.us-east-2
+  ami = data.aws_ami.ubuntu-east-2.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example-ec2"
+  }
+}
